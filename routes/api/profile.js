1ab58f7601a9225f9c9cd1357133fcb7096608f8
Piyush <piyushpachare1@gmail.com>
2020-08-11 20:51:16 +0530

    Profile model Added

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-//route for GET api/profile
+// @route   GET api/profile/me
+// @desc    Test route
+// @access  Public
 router.get("/", (req, res) => res.send("profile route"));
 
 module.exports = router;
